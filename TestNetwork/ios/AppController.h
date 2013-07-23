f5eb74a348cbb0c901d715745368cfafb94c23b6
Rick Cheng <rick.cheng@wemadeusa.com>
2013-07-23 16:12:40 -0700

    Initial Commit

---
+++ TestNetwork/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TestNetworkAppController.h
+//  TestNetwork
+//
+//  Created by Rickie Cheng on 7/23/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TestNetwork/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestNetworkAppController.h
+//  TestNetwork
+//
+//  Created by Rickie Cheng on 7/23/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
